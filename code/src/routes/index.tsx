a1a3a05e0622867c823bf699e6cb2c45b125f7d3
andrew cohen <cohen.andrew.n@gmail.com>
2024-08-20 15:06:34 -0400

    fix: spacing

--- code/src/routes/index.tsx
+++ code/src/routes/index.tsx
@@ -8,7 +8,7 @@ export default function Index() {
   return (
     <main class="p-4 justify-center items-center flex flex-col">
       <div class="p-4 w-96 flex flex-col items-center space-y-4">
-        <div class="text-center">
+        <div class="text-center h-fit">
           a simple way to let people know
           <br />
           you're thinking of them
@@ -17,13 +17,13 @@ export default function Index() {
         <div class="flex flex-col space-y-2 w-full">
           <a
             href="https://forms.gle/Y3cLNWGu2MoaYR1z5"
-            class="p-3 bg-rose-500 flex justify-center text-almond-50 rounded-xl text-xl w-full cursor-pointer"
+            class="p-3 bg-rose-500 flex justify-center text-almond-50 rounded-xl text-xl w-full cursor-pointer h-fit"
           >
             submit a hippo
           </a>
           <a
             href="/choose"
-            class="p-3 bg-matterhorn-900 flex justify-center text-almond-50 rounded-xl text-xl w-full cursor-pointer"
+            class="p-3 bg-matterhorn-900 flex justify-center text-almond-50 rounded-xl text-xl w-full cursor-pointer h-fit"
           >
             send a hippo
           </a>
